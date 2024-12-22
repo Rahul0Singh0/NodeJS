53ad147a6d10d33ca2aea199cc53a15546363db2
Rahul Singh <thesincererahul@gmail.com>
2024-12-22 17:45:36 +0530

    Added common js and module(ES6)

---
+++ 01/1_globals.js
@@ -0,0 +1,18 @@
+// Global Variables
+
+// __dirname
+console.log(__dirname); // It will print the directory path of the current file
+
+// module
+console.log(module); // It will print the module object
+
+console.log(__filename); // It will print the file path of the current file
+
+// process
+// Environment variables can be accessed through process.env
+console.log(process); // It will print the process object
+console.log(process.env); // It will print the environment variables
+
+console.log(require); // It will print the require function
+
+console.log(global); // setTimeout() etc..
---
+++ 01/main.mjs
@@ -0,0 +1,8 @@
+import { linearSearch, binarySearch } from "./2_searching.mjs";
+
+console.log(linearSearch([10, 20, 50, 30], 30));
+console.log(binarySearch([10, 20, 50, 30], 30));
+
+// const Searching = require('./2_searching.mjs');
+// console.log(Searching.linearSearch([10, 20, 50, 30], 30));
+// console.log(Searching.binarySearch([10, 20, 50, 30], 30));
