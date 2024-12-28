18b311f596d277fd45bca08fbb2d8a625968bdc1
Rahul Singh <thesincererahul@gmail.com>
2024-12-28 20:46:40 +0530

    updated

--- 05_TwitterApp/src/routes/V2/tweet.js
+++ 05_TwitterApp/src/routes/V2/tweet.js
@@ -4,13 +4,13 @@ const router = express.Router(); // create a new router object
 
 router.get('/', (req, res) => {
     return res.json({
-        message: 'Welcome to the tweet route'
+        message: 'Welcome to the tweet route v2'
     });
 });
 
 router.get('/:id', (req, res) => {
     return res.json({
-        message: 'Welcome to the tweet route',
+        message: 'Welcome to the tweet route v2',
         id: req.params.id
     });
 });
