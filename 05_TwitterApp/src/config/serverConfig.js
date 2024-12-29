78ef978005fa1a7990ded625edde8daed60e424c
Rahul Singh <thesincererahul@gmail.com>
2024-12-29 08:40:19 +0530

    .

--- 05_TwitterApp/src/config/serverConfig.js
+++ 05_TwitterApp/src/config/serverConfig.js
@@ -3,3 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config(); // Load environment variables from .env file
 
 export const PORT = process.env.PORT || 3000; // Define a PORT variable
+
+export const MONGODB_URL = process.env.MONGODB_URL; // Define a MONGODB_URL variable
