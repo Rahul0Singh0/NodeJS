28d5b6255982690b4076fc13fba14bfeb389c3d1
Rahul Singh <thesincererahul@gmail.com>
2024-12-29 13:46:37 +0530

    flow of modules

--- 05_TwitterApp/README.md
+++ 05_TwitterApp/README.md
@@ -2,3 +2,5 @@
 [click](https://www.npmjs.com/package/zod)
 
 TypeScript-first schema validation with static type inference
+
+![alt text](image.png)
--- 05_TwitterApp/src/services/tweetService.js
+++ 05_TwitterApp/src/services/tweetService.js
@@ -2,10 +2,10 @@ import { Filter } from "bad-words"
 import { createTweet as createTweetRepository } from "../repositories/tweetRepository.js";
 
 export const createTweet = async ({ body }) => {
-    // try to find blocked words in the tweet body and if any exists don't
-    // create the tweet and return an error message
+    // try to find blocked words in the tweet body and
+    //  if any exists don't create the tweet and return
+    //  an error message
     const filter = new Filter();
-
     if (filter.isProfane(body)) {
         console.log(body);
         console.log(filter.clean(body));
@@ -14,7 +14,6 @@ export const createTweet = async ({ body }) => {
             status: 400
         };
     }
-
     const tweet = await createTweetRepository({ body });
     return tweet;
 }
